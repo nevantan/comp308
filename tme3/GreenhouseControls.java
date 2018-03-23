09e57643245f74394bfdd322d4a066a67d34d14d
Nevan Tan <nevan@tanclan.ca>
2018-03-23 14:47:41 -0400

    Format code

--- tme3/GreenhouseControls.java
+++ tme3/GreenhouseControls.java
@@ -36,167 +36,193 @@ import java.util.Calendar;
 import tme3.*;
 
 public class GreenhouseControls extends Controller {
-  private boolean light = false;
-  private boolean water = false;
-  private boolean fans = false;
-  private String thermostat = "Day";
-  private String eventsFile = "examples1.txt";
+    private boolean light = false;
+    private boolean water = false;
+    private boolean fans = false;
+    private String thermostat = "Day";
+    private String eventsFile = "examples1.txt";
 
 
-  public class LightOn extends Event {
-    public LightOn(long delayTime) { super(delayTime); }
-    public void action() {
-      // Put hardware control code here to
-      // physically turn on the light.
-      light = true;
-    }
-    public String toString() { return "Light is on"; }
-  }
-  
-  public class LightOff extends Event {
-    public LightOff(long delayTime) { super(delayTime); }
-    public void action() {
-      // Put hardware control code here to
-      // physically turn off the light.
-      light = false;
-    }
-    public String toString() { return "Light is off"; }
-  }
-  
-  public class WaterOn extends Event {
-    public WaterOn(long delayTime) { super(delayTime); }
-    public void action() {
-      // Put hardware control code here.
-      water = true;
-    }
-    public String toString() {
-      return "Greenhouse water is on";
-    }
-  }
-  
-  public class WaterOff extends Event {
-    public WaterOff(long delayTime) { super(delayTime); }
-    public void action() {
-      // Put hardware control code here.
-      water = false;
-    }
-    public String toString() {
-      return "Greenhouse water is off";
-    }
-  }
-  
-  public class FansOn extends Event {
-  	public FansOn(long delayTime) { super(delayTime); }
-  	public void action() {
-  		fans = true;
-  	}
-  	public String toString() {
-  		return "Greenhouse fans are on";
-  	}
-  }
-  
-  public class FansOff extends Event {
-  	public FansOff(long delayTime) { super(delayTime); }
-  	public void action() {
-  		fans = true;
-  	}
-  	public String toString() {
-  		return "Greenhouse fans are off";
-  	}
-  }
-  
-  public class ThermostatNight extends Event {
-    public ThermostatNight(long delayTime) {
-      super(delayTime);
-    }
-    public void action() {
-      // Put hardware control code here.
-      thermostat = "Night";
-    }
-    public String toString() {
-      return "Thermostat on night setting";
-    }
-  }
-  
-  public class ThermostatDay extends Event {
-    public ThermostatDay(long delayTime) {
-      super(delayTime);
-    }
-    public void action() {
-      // Put hardware control code here.
-      thermostat = "Day";
-    }
-    public String toString() {
-      return "Thermostat on day setting";
-    }
-  }
-  
-  // An example of an action() that inserts a
-  // new one of itself into the event list:
-  public class Bell extends Event {
-    public Bell(long delayTime) { super(delayTime); }
-    public void action() {
-	// nothing to do
-    }
-    public String toString() { return "Bing!"; }
-  }
-
-  public class Restart extends Event {
-    public Restart(long delayTime, String filename) {
-      super(delayTime);
-      eventsFile = filename;
+    public class LightOn extends Event {
+        public LightOn(long delayTime) {
+            super(delayTime);
+        }
+        public void action() {
+            // Put hardware control code here to
+            // physically turn on the light.
+            light = true;
+        }
+        public String toString() {
+            return "Light is on";
+        }
     }
 
-    public void action() {
-	addEvent(new ThermostatNight(0));
-	addEvent(new LightOn(2000));
-	addEvent(new WaterOff(8000));
-	addEvent(new ThermostatDay(10000));
-	addEvent(new Bell(9000));
-	addEvent(new WaterOn(6000));
-	addEvent(new LightOff(4000));
-	addEvent(new Terminate(12000));
+    public class LightOff extends Event {
+        public LightOff(long delayTime) {
+            super(delayTime);
+        }
+        public void action() {
+            // Put hardware control code here to
+            // physically turn off the light.
+            light = false;
+        }
+        public String toString() {
+            return "Light is off";
+        }
     }
 
-    public String toString() {
-      return "Restarting system";
+    public class WaterOn extends Event {
+        public WaterOn(long delayTime) {
+            super(delayTime);
+        }
+        public void action() {
+            // Put hardware control code here.
+            water = true;
+        }
+        public String toString() {
+            return "Greenhouse water is on";
+        }
     }
-  }
 
-  public class Terminate extends Event {
-    public Terminate(long delayTime) { super(delayTime); }
-    public void action() { System.exit(0); }
-    public String toString() { return "Terminating";  }
-  }
+    public class WaterOff extends Event {
+        public WaterOff(long delayTime) {
+            super(delayTime);
+        }
+        public void action() {
+            // Put hardware control code here.
+            water = false;
+        }
+        public String toString() {
+            return "Greenhouse water is off";
+        }
+    }
 
-  public static void printUsage() {
-    System.out.println("Correct format: ");
-    System.out.println("  java GreenhouseControls -f <filename>, or");
-    System.out.println("  java GreenhouseControls -d dump.out");
-  }
+    public class FansOn extends Event {
+        public FansOn(long delayTime) {
+            super(delayTime);
+        }
+        public void action() {
+            fans = true;
+        }
+        public String toString() {
+            return "Greenhouse fans are on";
+        }
+    }
 
-//---------------------------------------------------------
+    public class FansOff extends Event {
+        public FansOff(long delayTime) {
+            super(delayTime);
+        }
+        public void action() {
+            fans = true;
+        }
+        public String toString() {
+            return "Greenhouse fans are off";
+        }
+    }
 
-  public static void main(String[] args) {
-	try {
-	    String option = args[0];
-	    String filename = args[1];
+    public class ThermostatNight extends Event {
+        public ThermostatNight(long delayTime) {
+            super(delayTime);
+        }
+        public void action() {
+            // Put hardware control code here.
+            thermostat = "Night";
+        }
+        public String toString() {
+            return "Thermostat on night setting";
+        }
+    }
 
-	    if ( !(option.equals("-f")) && !(option.equals("-d")) ) {
-		System.out.println("Invalid option");
-		printUsage();
-	    }
+    public class ThermostatDay extends Event {
+        public ThermostatDay(long delayTime) {
+            super(delayTime);
+        }
+        public void action() {
+            // Put hardware control code here.
+            thermostat = "Day";
+        }
+        public String toString() {
+            return "Thermostat on day setting";
+        }
+    }
 
-	    GreenhouseControls gc = new GreenhouseControls();
+    // An example of an action() that inserts a
+    // new one of itself into the event list:
+    public class Bell extends Event {
+        public Bell(long delayTime, int rings) {
+            super(delayTime);
+        }
+        public void action() {
+            // nothing to do
+        }
+        public String toString() {
+            return "Bing!";
+        }
+    }
 
-	    if (option.equals("-f"))  {
-		gc.addEvent(gc.new Restart(0,filename));
-	    }
+    public class Restart extends Event {
+        public Restart(long delayTime, String filename) {
+            super(delayTime);
+            eventsFile = filename;
+        }
 
-	    gc.run();
-	} catch (ArrayIndexOutOfBoundsException e) {
-	    System.out.println("Invalid number of parameters");
-	    printUsage();
-	}
-  }
+        public void action() {
+            addEvent(new ThermostatNight(0));
+            addEvent(new LightOn(2000));
+            addEvent(new WaterOff(8000));
+            addEvent(new ThermostatDay(10000));
+            addEvent(new Bell(9000));
+            addEvent(new WaterOn(6000));
+            addEvent(new LightOff(4000));
+            addEvent(new Terminate(12000));
+        }
+
+        public String toString() {
+            return "Restarting system";
+        }
+    }
+
+    public class Terminate extends Event {
+        public Terminate(long delayTime) {
+            super(delayTime);
+        }
+        public void action() {
+            System.exit(0);
+        }
+        public String toString() {
+            return "Terminating";
+        }
+    }
+
+    public static void printUsage() {
+        System.out.println("Correct format: ");
+        System.out.println("  java GreenhouseControls -f <filename>, or");
+        System.out.println("  java GreenhouseControls -d dump.out");
+    }
+
+    //---------------------------------------------------------
+
+    public static void main(String[] args) {
+        try {
+            String option = args[0];
+            String filename = args[1];
+
+            if (!(option.equals("-f")) && !(option.equals("-d"))) {
+                System.out.println("Invalid option");
+                printUsage();
+            }
+
+            GreenhouseControls gc = new GreenhouseControls();
+
+            if (option.equals("-f")) {
+                gc.addEvent(gc.new Restart(0, filename));
+            }
+
+            gc.run();
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid number of parameters");
+            printUsage();
+        }
+    }
 } ///:~
